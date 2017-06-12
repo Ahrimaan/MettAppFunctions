df21b2d7a5dcccf4c37a07011ead5a0aaf5b1b6f
Ahrimaan <warmuth.pawel@gmail.com>
2017-06-12 15:44:42 +0200

    Added Check for all Fields

--- AddEvent/index.js
+++ AddEvent/index.js
@@ -1,20 +1,32 @@
 var tokenCheck = require('../checkToken');
+var request = require('request');
 
 module.exports = function (context, req, doc) {
     context.log('Token :' + req.headers.token);
     if (tokenCheck(req.headers.token, context)) {
-        let newEventDate = new Date(req.body.eventDate);
+        let newEventDate = (req.body && req.body.eventDate) ?
+            new Date(req.body.eventDate) : undefined;
+        let newEvent = {
+            eventDate: newEventDate,
+            createDate: new Date(),
+            createdBy: 'TODO',
+            participants: []
+        }
+        if (!allInformationProvided(newEvent)) {
+            context.res = {
+                status: 400,
+                body: {
+                    message: ' not all fields are filled '
+                }
+            };
+            context.done();
+            return;
+        }
         if (isEventAvailable(doc, newEventDate)) {
-            let newEvent = {
-                eventDate:newEventDate,
-                createDate:new Date(),
-                createdBy:'TODO',
-                participants:[]
-            }
             context.bindings.event = newEvent;
             context.res = {
                 body: context.bindings.event
-            };
+            }
         } else {
             context.res = {
                 status: 409,
@@ -26,6 +38,11 @@ module.exports = function (context, req, doc) {
     }
     context.done();
 };
+
+function allInformationProvided(newEvent) {
+    return newEvent.eventDate !== undefined &&
+        newEvent.createdBy !== undefined;
+}
 
 function isEventAvailable(doc, eventDate) {
     let filtered = doc.filter(item => new Date(item.EventDate).getDate() === eventDate.getDate());
