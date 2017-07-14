bed5439e2c0e9eeefd9bdeb60f5b7ce288610c23
Pawel <warmuth.pawel@gmail.com>
2017-07-14 13:06:29 +0200

    Added Unscribe Event

---
+++ UnscribeEvent/index.js
@@ -0,0 +1,86 @@
+var tokenCheck = require('../checkToken');
+var userService = require('../userService');
+var DocumentClient = require('documentdb').DocumentClient;
+var client = new DocumentClient(process.env.dbHost, { masterKey: process.env.dbMasterKey });
+var docDb = require('../docDbHelper');
+
+module.exports = function (context, req, doc) {
+    context.log('Token: ' + req.headers.token);
+    let userToken = tokenCheck(req.headers.token, context)
+    if (userToken) {
+        userService.getUserInformation(userToken.sub, (err, result) => {
+            if (err) {
+                context.res = {
+                    status: 500,
+                    body: err
+                };
+                context.done();
+            }
+            let participantData = {
+                userId: result.user_id,
+                userName: result.name,
+                value: req.body.value,
+                specialNeeds: req.body.special,
+                payed: req.body.payed
+            }
+            if (doc.filter(x => x._self === req.body.eventId).length <= 0) {
+                context.res = {
+                    status: 404
+                };
+                context.done();
+            }
+            if (doc.filter(x => x._self === req.body.eventId)[0].participants.filter(part => part.userId === result.user_id).length <= 0) {
+                context.res = {
+                    status:'409',
+                    body: {
+                        'message':'you are not subscribed, dont try to call my api by hand ;) '
+                    }
+                }
+                context.done();
+            } else {
+                updateItem(req.body.eventId, participantData, (err, result) => {
+                    if (err) {
+                        context.res = {
+                            status: 500,
+                            body: err
+                        };
+                        context.done();
+                    } else {
+                        context.res = {
+                            body: result
+                        };
+                        context.done();
+                    }
+                });
+            }
+        });
+    }
+    else {
+        context.res = {
+            status: 401,
+            body: 'token was wrong expired or missing'
+        };
+        context.done();
+    }
+};
+
+function updateItem(eventId, participant, callback) {
+    client.readDocument(eventId, (err, result) => {
+        if (err) {
+            callback(err);
+        }
+        else {
+            let event = result;
+            event.partticipants =  event.participants.splice(event.participants.findIndex(part => part.userId === participant.user_id),1);
+
+            client.replaceDocument(eventId, event, (repErr, repRes) => {
+                if (repErr) {
+                    callback(repErr);
+                }
+                else {
+                    callback(null, repRes);
+                }
+            });
+        }
+    });
+}
